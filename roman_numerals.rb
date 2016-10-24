e8fd94c7ffb26d1383642c9cff0288baf3d33fe6
Jordan Munoz & Nhan Nguyen <pair+jtmunoz.nhantnon@devbootcamp.com>
2016-10-24 15:24:27 -0700

    add roman numeral hash and key values. set up if else conditions to print numbers in between the ranges

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -2,13 +2,36 @@
 #takes the integer and creates a string of that length
 #with the roman numerals
 def convert_to_roman(arabic_number)
-#
-#if arabic_number < 5
-  roman_number = "I" * arabic_number
-#elsif arabic_number == 5
-#  roman_number = "V"
-#else
-return roman_number
+
+hash_number = {
+  "1" => "I",
+  "5" => "V",
+  "10" => "X",
+  "50" => "L",
+  "100" => "C",
+  "500" => "D",
+  "1000" => "M"
+}
+arabic_number = 9
+#hash_number["#{arabic_number}"]
+
+
+if arabic_number >= 1 && arabic_number < 5
+  arabic_number.times do |x|
+  print hash_number["1"]
+  end
+elsif
+  arabic_number > 5 && arabic_number < 10
+  remainder = arabic_number%5
+  remainder.times do |x|
+  print hash_number["1"]
+  end
+
+
+
+
+
+
 end
 #
 #
