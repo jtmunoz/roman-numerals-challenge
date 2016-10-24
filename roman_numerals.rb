eb996c0ada097dd7dffd517bdbce8be520f0c49b
Benjamin Kwok & Patrick Anderson <pair+kwokhuen.pand0@devbootcamp.com>
2016-10-24 12:16:49 -0700

    add convert_to_roman method to take a number and return roman numeral

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,3 +1,10 @@
 def convert_to_roman(arabic_number)
-  # Your code goes here
+  output = ''
+  while arabic_number > 0
+    output << "I"
+    arabic_number -= 1
+  end
+  output
 end
+
+p convert_to_roman(4)
