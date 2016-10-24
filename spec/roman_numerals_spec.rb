3ccd955675c173c25d53850bbddacb72e0f5c8c5
Mark Siemers <mark.siemers@gmail.com>
2016-10-24 16:23:12 -0700

    Add tests for modern roman numerals

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -81,8 +81,35 @@ describe 'converting an Arabic number to a Roman numeral' do
   end
 
   describe 'modern Roman numerals' do
-    # Release 3 ...
-    # add tests for modern roman numerals here
-
+    it 'converts 4 to IV' do
+      expect(convert_to_roman(4, modern: true)).to eq "IV"
+    end
+    it 'converts 9 to IX' do
+      expect(convert_to_roman(9, modern: true)).to eq "IX"
+    end
+    it 'converts 14 to XIV' do
+      expect(convert_to_roman(14, modern: true)).to eq "XIV"
+    end
+    it 'converts 44 to XLIV' do
+      expect(convert_to_roman(44, modern: true)).to eq "XLIV"
+    end
+    it 'converts 49 to XLIX' do
+      expect(convert_to_roman(49, modern: true)).to eq "XLIX"
+    end
+    it 'converts 99 to XCIX' do
+      expect(convert_to_roman(99, modern: true)).to eq "XCIX"
+    end
+    it 'converts 400 to CD' do
+      expect(convert_to_roman(400, modern: true)).to eq "CD"
+    end
+    it 'converts 499 to CDXCIX' do
+      expect(convert_to_roman(499, modern: true)).to eq "CDXCIX"
+    end
+    it 'converts 944 to CMXLIV' do
+      expect(convert_to_roman(944, modern: true)).to eq "CMXLIV"
+    end
+    it 'converts 999 to CMXCIX' do
+      expect(convert_to_roman(999, modern: true)).to eq "CMXCIX"
+    end
   end
 end
