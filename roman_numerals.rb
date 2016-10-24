9950768721525a7cb0ac37987166058d2fd84212
Jordan Munoz & Nhan Nguyen <pair+jtmunoz.nhantnon@devbootcamp.com>
2016-10-24 14:26:22 -0700

    adds conversion for arabic numeral to roman numeral from 1 to 4

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,3 +1,25 @@
+#method takes an arabic number converts it into an integer
+#takes the integer and creates a string of that length
+#with the roman numerals
 def convert_to_roman(arabic_number)
-  # Your code goes here
+#
+#if arabic_number < 5
+  roman_number = "I" * arabic_number
+#elsif arabic_number == 5
+#  roman_number = "V"
+#else
+return roman_number
 end
+#
+#
+#
+#
+#puts "Please enter a number:"
+#arabic_number = gets.chomp.to_i
+#
+#convert_to_roman(arabic_number)
+#
+#number 3 is passed in
+# 3 * roman_number
+
+
