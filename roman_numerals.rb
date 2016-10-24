c2cbb3c7f522645bf0877e01b1c87829a02724f8
Conor Burke & Kelsey <pair+conorburke.kedskeds@devbootcamp.com>
2016-10-24 12:41:35 -0700

    Write methods and tests for all roman numerals

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,7 +1,39 @@
-def convert_to_roman(arabic_number)
-  if arabic_number == 4
-    return "IIII"
-  else
-    return "I"
+def helper(symbol,num)
+  str = ""
+  num.times do
+    str += symbol
   end
+  return str
 end
+
+def convert_to_roman(arabic_number)
+  str = ""
+  loop do
+    if arabic_number / 1000 >= 1
+      str+= "M"
+      arabic_number -= 1000
+    elsif arabic_number / 500 >= 1
+      str+= "D"
+      arabic_number -= 500
+    elsif arabic_number / 100 >= 1
+      str+= "C"
+      arabic_number -= 100
+    elsif arabic_number / 50 >= 1
+      str+= "L"
+      arabic_number -= 50
+    elsif arabic_number / 10 >= 1
+      str+= "X"
+      arabic_number -= 10
+    elsif arabic_number / 5 >= 1
+      str+= "V"
+      arabic_number -= 5
+    else
+      str+= helper("I",arabic_number)
+      arabic_number = 0
+    end
+    break if arabic_number == 0
+  end
+  return str
+end
+
+convert_to_roman(20)
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -12,6 +12,29 @@ describe 'converting an Arabic number to a Roman numeral' do
 
     # Release 1 ...
     # add tests for old roman numerals here
+    it 'converts 5 to V' do
+      expect(convert_to_roman(5)).to eq "V"
+    end
+
+    it 'converts 9 to VIIII' do
+      expect(convert_to_roman(9)).to eq "VIIII"
+    end
+
+    it 'converts 20 to XX' do
+      expect(convert_to_roman(20)).to eq "XX"
+    end
+
+    it 'converts 123 to CXXIII' do
+      expect(convert_to_roman(123)).to eq "CXXIII"
+    end
+
+    it 'converts 2337 to MMCCCXXXVII' do
+      expect(convert_to_roman(2337)).to eq "MMCCCXXXVII"
+    end
+
+    it 'converts 500 to D' do
+      expect(convert_to_roman(500)).to eq "D"
+    end
 
   end
 
