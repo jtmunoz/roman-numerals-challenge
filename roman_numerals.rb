1bfa72d06a07e4ab82b2bbb5029e6305b6a12620
Benjamin Kwok & Patrick Anderson <pair+kwokhuen.pand0@devbootcamp.com>
2016-10-24 12:46:20 -0700

    modified convert_to_roman method to convert arabic number to roman numerals for any arabic number

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,10 +1,51 @@
 def convert_to_roman(arabic_number)
   output = ''
   while arabic_number > 0
-    output << "I"
-    arabic_number -= 1
+
+    i = arabic_number / 1000
+    arabic_number = arabic_number % 1000
+    i.times do
+      output << "M"
+    end
+
+    i = arabic_number / 500
+    arabic_number = arabic_number % 500
+    i.times do
+      output << "D"
+    end
+
+    i = arabic_number / 100
+    arabic_number = arabic_number % 100
+    i.times do
+      output << "C"
+    end
+
+    i = arabic_number / 50
+    arabic_number = arabic_number % 50
+    i.times do
+      output << "L"
+    end
+
+    i = arabic_number / 10
+    arabic_number = arabic_number % 10
+    i.times do
+      output << "X"
+    end
+
+    i = arabic_number / 5
+    arabic_number = arabic_number % 5
+    i.times do
+      output << "V"
+    end
+
+    i = arabic_number / 1
+    arabic_number = arabic_number % 1
+    i.times do
+      output << "I"
+    end
+
   end
   output
 end
 
-p convert_to_roman(4)
+p convert_to_roman(2554)
