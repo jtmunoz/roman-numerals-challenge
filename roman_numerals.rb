06e305b5cd52d9de9cfbec094a8bb3b5a25d51a3
Jordan Munoz & Nhan Nguyen <pair+jtmunoz.nhantnon@devbootcamp.com>
2016-10-24 16:23:30 -0700

    adds more conditions to roman_numerals

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,7 +1,10 @@
 #method takes an arabic number converts it into an integer
 #takes the integer and creates a string of that length
 #with the roman numerals
+
+
 def convert_to_roman(arabic_number)
+
 
 hash_number = {
   "1" => "I",
@@ -12,27 +15,52 @@ hash_number = {
   "500" => "D",
   "1000" => "M"
 }
-arabic_number = 9
+
 #hash_number["#{arabic_number}"]
-
-
 if arabic_number >= 1 && arabic_number < 5
   arabic_number.times do |x|
   print hash_number["1"]
   end
 elsif
+  arabic_number == 5
+  print hash_number["5"]
+elsif
   arabic_number > 5 && arabic_number < 10
   remainder = arabic_number%5
+  print hash_number["5"]
   remainder.times do |x|
   print hash_number["1"]
   end
-
-
-
-
-
-
+elsif
+  arabic_number == 10
+  print hash_number["10"]
+elsif
+  arabic_number > 10 && arabic_number < 50
+  remainder = arabic_number/10
+  remainder.times do |x|
+  print hash_number["10"]
+  end
+  other = arabic_number%10
+  print convert_to_roman(other)
+elsif
+  arabic_number == 50
+  print hash_number["50"]
+elsif
+  arabic_number > 50 && arabic_number < 100
+  remainder = arabic_number/50
+  remainder.times do |x|
+  print hash_number["50"]
+  end
+  other = arabic_number%50
+  print convert_to_roman(other)
+else
 end
+end
+
+
+
+
+
 #
 #
 #
