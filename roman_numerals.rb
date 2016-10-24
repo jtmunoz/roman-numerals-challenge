68aebc16ea877c45ea66ead5711a34496e414825
Christine Duane & Christopher Mendoza <pair+ckduane.wushuchris@devbootcamp.com>
2016-10-24 13:31:52 -0700

    finished release 0

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,3 +1,25 @@
 def convert_to_roman(arabic_number)
-  # Your code goes here
+
+
+  return_array = []
+  old_roman = {
+    1 => "I",
+    5 => "V",
+    10 => "X",
+    50 => "L",
+    100 => "C",
+    500 => "D",
+    1000 => "M"
+  }
+
+
+
+  if arabic_number < 5
+    arabic_number.times do
+      return_array << "I"
+    end
+  end
+
+ return_array.join
+
 end
