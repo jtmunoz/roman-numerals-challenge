07552e874282b8005664fbc7c7546e222b8f4744
Qiaoshu Lin & Chinh Nguyen & Omar <pair+chelseylin.chqnguyen.omarchaeopteryx@devbootcamp.com>
2016-10-24 14:24:27 -0700

    add mordern roman numeral option and test

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,25 +1,43 @@
-def convert_to_roman(arabic_number)
+def convert_to_roman(arabic_number, options = {})
   # Your code goes here
   result = ""
   while arabic_number > 0
     if arabic_number >= 1000
       result += "M"
       arabic_number -= 1000
+      elsif (arabic_number >= 900) && (options[:modern] == true)
+      result += "CM"
+      arabic_number -= 900
       elsif arabic_number >= 500
       result += "D"
       arabic_number -= 500
+      elsif (arabic_number >= 400) && (options[:modern] == true)
+      result += "CD"
+      arabic_number -= 400
       elsif arabic_number >= 100
       result += "C"
       arabic_number -= 100
+      elsif (arabic_number >= 90) && (options[:modern] == true)
+      result += "XC"
+      arabic_number -= 90
       elsif arabic_number >= 50
       result += "L"
       arabic_number -= 50
+      elsif (arabic_number >= 40) && (options[:modern] == true)
+      result += "XL"
+      arabic_number -= 40
       elsif arabic_number >= 10
       result += "X"
       arabic_number -= 10
+      elsif (arabic_number >= 9) && (options[:modern] == true)
+      result += "IX"
+      arabic_number -= 9
       elsif arabic_number >= 5
       result += "V"
       arabic_number -= 5
+      elsif (arabic_number >= 4) && (options[:modern] == true)
+      result += "IV"
+      arabic_number -= 4
       elsif arabic_number >= 1
       result += "I"
       arabic_number -= 1
@@ -30,3 +48,4 @@ end
 
 
 convert_to_roman(66)
+convert_to_roman(2016, {modern: true})
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -12,12 +12,28 @@ describe 'converting an Arabic number to a Roman numeral' do
 
     # Release 1 ...
     # add tests for old roman numerals here
+    it 'converts 9 to VIIII' do
+      expect(convert_to_roman(9)).to eq "VIIII"
+    end
+
+    it 'converts 5 to V' do
+      expect(convert_to_roman(5)).to eq "V"
+    end
 
   end
 
   describe 'modern Roman numerals' do
     # Release 3 ...
     # add tests for modern roman numerals here
+    it 'converts 49 to XLIX' do
+      expect(convert_to_roman(49, { modern: true})).to eq "XLIX"
+    end
+    it 'converts 2016 to MMXVI' do
+      expect(convert_to_roman(2016, { modern: true})).to eq "MMXVI"
+    end
+    it 'converts 489 to CDLXXXIX' do
+      expect(convert_to_roman(489, { modern: true})).to eq "CDLXXXIX"
+    end
 
   end
 end
