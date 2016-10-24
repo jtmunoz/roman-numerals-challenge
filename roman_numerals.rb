1e71dcadf128f1e410d5770fbe099ec202a98a81
Christine Duane & Christopher Mendoza <pair+ckduane.wushuchris@devbootcamp.com>
2016-10-24 16:14:43 -0700

    release 1 complete

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -11,14 +11,60 @@ def convert_to_roman(arabic_number)
     500 => "D",
     1000 => "M"
   }
-
-
-
-  if arabic_number < 5
-    arabic_number.times do
-      return_array << "I"
+  #in the hash, return value
+  if old_roman.has_key?(arabic_number)
+    return_array << old_roman[arabic_number]
+  else
+    number_array = arabic_number.to_s.split("")
+    #ones place
+    if number_array[-1].to_i >= 5
+      return_array.push("V")
+      (number_array[-1].to_i%5).times do
+        return_array << "I"
+      end
+    else
+      (number_array[-1].to_i%5).times do
+        return_array << "I"
+      end
     end
+
+    #tens place
+    if number_array[-2] != nil
+      if number_array[-2].to_i >= 5
+        return_array.unshift("L")
+        (number_array[-2].to_i%5).times do
+          return_array.insert(1,"X")
+        end
+      else
+        (number_array[-2].to_i%5).times do
+        return_array.unshift("X")
+        end
+      end
+    end
+
+    #hundreds place
+    if number_array[-3] != nil
+      if number_array[-3].to_i >= 5
+        return_array.unshift("D")
+        (number_array[-3].to_i%5).times do
+          return_array.insert(1,"C")
+        end
+      else
+        (number_array[-3].to_i%5).times do
+        return_array.unshift("C")
+        end
+      end
+    end
+
+    #thousands place
+    if number_array[-4] != nil
+      (number_array[-4].to_i).times do
+        return_array.insert(0,"M")
+      end
+    end
+
   end
+
 
  return_array.join
 
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -12,6 +12,43 @@ describe 'converting an Arabic number to a Roman numeral' do
 
     # Release 1 ...
     # add tests for old roman numerals here
+    it 'converts 5 to V' do
+      expect(convert_to_roman(5)).to eq "V"
+    end
+
+    it 'converts 6 to VI' do
+      expect(convert_to_roman(6)).to eq "VI"
+    end
+
+    it 'converts 12 to XII' do
+      expect(convert_to_roman(12)).to eq "XII"
+    end
+
+    it 'converts 16 to XVI' do
+      expect(convert_to_roman(16)).to eq "XVI"
+    end
+
+    it 'converts 965 to DCCCCLXV' do
+      expect(convert_to_roman(965)).to eq "DCCCCLXV"
+    end
+
+    it 'converts 963 to DCCCCLXIII' do
+      expect(convert_to_roman(963)).to eq "DCCCCLXIII"
+    end
+
+    it 'converts 967 to DCCCCLXVII' do
+      expect(convert_to_roman(967)).to eq "DCCCCLXVII"
+    end
+
+    it 'converts 2965 to MMDCCCC' do
+      expect(convert_to_roman(2900)).to eq "MMDCCCC"
+    end
+
+    it 'converts 3000 to MMM' do
+      expect(convert_to_roman(3000)).to eq "MMM"
+    end
+
+
 
   end
 
