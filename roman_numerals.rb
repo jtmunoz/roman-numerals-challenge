d5048ae7f9186c3669c46afeb38c5f69a41d00c2
Ian Judson & Tae Yun <pair+Ijud.yuntsj@devbootcamp.com>
2016-10-24 14:29:36 -0700

    creating loop, running less than 10

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,3 +1,28 @@
 def convert_to_roman(arabic_number)
   # Your code goes here
+  counter = arabic_number
+  rom_num = ""
+ # create hash for reference
+  num_hash = {
+    1 => "I",
+    5 => "V",
+    10 => "X",
+    50 => "L",
+    100 => "C",
+    500 => "D",
+    1000 => "M"
+  }
+ while counter > 0
+    if counter >= 1 && counter < 5
+      rom_num += num_hash[1]
+      counter -= 1
+    elsif counter >= 5 && counter < 10
+      rom_num += num_hash[5]
+      counter -= 5
+    end
+ end
+
+ rom_num
 end
+
+p convert_to_roman(9)
