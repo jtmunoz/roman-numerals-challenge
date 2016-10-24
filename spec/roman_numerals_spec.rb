1e9fdce11711f1583e30552516ffb958d4b00009
Benjamin Kwok & Patrick Anderson <pair+kwokhuen.pand0@devbootcamp.com>
2016-10-24 12:47:13 -0700

    add method to test the convert_to_roman method

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -12,6 +12,17 @@ describe 'converting an Arabic number to a Roman numeral' do
 
     # Release 1 ...
     # add tests for old roman numerals here
+    it 'converts 5 to V' do
+      expect(convert_to_roman(5)).to eq "V"
+    end
+
+    it 'converts 6 to VI' do
+      expect(convert_to_roman(6)).to eq "VI"
+    end
+
+    it 'converts 10 to X' do
+      expect(convert_to_roman(10)).to eq "X"
+    end
 
   end
 
