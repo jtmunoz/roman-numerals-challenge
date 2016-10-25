d11ec9dfaffdcbb072ad970f82f1eddd5ff87108
Jordan Munoz & Nhan Nguyen <pair+jtmunoz.nhantnon@devbootcamp.com>
2016-10-24 17:05:27 -0700

    add a result container to keep track of the roman numeral conversions

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,60 +1,62 @@
 #method takes an arabic number converts it into an integer
 #takes the integer and creates a string of that length
 #with the roman numerals
-
+require 'pry'
 
 def convert_to_roman(arabic_number)
 
+ result = ''
 
-hash_number = {
-  "1" => "I",
-  "5" => "V",
-  "10" => "X",
-  "50" => "L",
-  "100" => "C",
-  "500" => "D",
-  "1000" => "M"
-}
+  hash_number = {
+    "1" => "I",
+    "5" => "V",
+    "10" => "X",
+    "50" => "L",
+    "100" => "C",
+    "500" => "D",
+    "1000" => "M"
+  }
 
-#hash_number["#{arabic_number}"]
-if arabic_number >= 1 && arabic_number < 5
-  arabic_number.times do |x|
-  print hash_number["1"]
+  while arabic_number > 0
+    if arabic_number >= 1 && arabic_number < 5
+
+      arabic_number.times do |x|
+        arabic_number = 0
+        result += hash_number["1"]
+      end
+
+    elsif arabic_number == 5
+      arabic_number = arabic_number - 5
+      result += hash_number["5"]
+    elsif arabic_number > 5 && arabic_number < 10
+      arabic_number = arabic_number - 5
+      result += hash_number["5"]
+
+    elsif arabic_number == 10
+      arabic_number = arabic_number - 10
+      result += hash_number["10"]
+    elsif arabic_number > 10 && arabic_number < 50
+      multiplier = arabic_number / 10
+
+      multiplier.times do |x|
+        arabic_number = arabic_number - 10
+        result += hash_number["10"]
+      end
+
+    elsif arabic_number == 50
+      arabic_number = arabic_number - 50
+      result += hash_number["50"]
+    elsif arabic_number > 50 && arabic_number < 100
+      multiplier = arabic_number / 50
+
+      multiplier.times do |x|
+        arabic_number = arabic_number - 50
+        result +=  hash_number["50"]
+      end
+    else
+    end
   end
-elsif
-  arabic_number == 5
-  print hash_number["5"]
-elsif
-  arabic_number > 5 && arabic_number < 10
-  remainder = arabic_number%5
-  print hash_number["5"]
-  remainder.times do |x|
-  print hash_number["1"]
-  end
-elsif
-  arabic_number == 10
-  print hash_number["10"]
-elsif
-  arabic_number > 10 && arabic_number < 50
-  remainder = arabic_number/10
-  remainder.times do |x|
-  print hash_number["10"]
-  end
-  other = arabic_number%10
-  print convert_to_roman(other)
-elsif
-  arabic_number == 50
-  print hash_number["50"]
-elsif
-  arabic_number > 50 && arabic_number < 100
-  remainder = arabic_number/50
-  remainder.times do |x|
-  print hash_number["50"]
-  end
-  other = arabic_number%50
-  print convert_to_roman(other)
-else
-end
+  result
 end
 
 
